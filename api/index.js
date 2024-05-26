063d58844b506c3f7c5bceadf5d608ba73ea1716
Kapileswar Rout <kapileswarrout143@gmail.com>
2024-05-27 00:57:35 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config()
+
+const app = express();
+app.use(express.json()); //by default json cannot be send to server so this will allow it
 
 mongoose.connect(process.env.MONGO).then( () => {
     console.log('Connected to mongodb')
@@ -11,10 +15,11 @@ mongoose.connect(process.env.MONGO).then( () => {
     console.log(err);
 })
 
-const app = express();
+
 
 app.listen(3000, ()=>{
     console.log("server is running at port 3000!")
 })
 
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
