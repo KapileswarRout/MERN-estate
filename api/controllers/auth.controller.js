0ce407bf56d6ab66b58f50d444d02ad3f5c9c291
Kapileswar Rout <kapileswarrout143@gmail.com>
2024-05-28 01:49:49 +0530

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,15 +16,15 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body; //destructuring of email password
   try {
     const validUser = await User.findOne({ email }); //findOne method is used on User model to search for the email
     if (!validUser) return next(errorHandler(404, "User not found!")); //middleware uses the custom error to show in UI
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //a unique thing to the user is added to token which is _id in mongoDB 
-    //remove sensitive information
-    const {password: pass, ...userWithoutPassword} = validUser._doc
+    //remove sensitive information from raw data
+    const {password: pass, ...userWithoutPassword} = validUser._doc 
     res
       .cookie("accessToken", token, { httpOnly: true }) //no other thirdparty app can have acces to our cookie by httpOnly 
       .status(200)
