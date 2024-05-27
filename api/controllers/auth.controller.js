9d5b3dfc5387604175b6002c6d0bcd35b84cb070
Kapileswar Rout <kapileswarrout143@gmail.com>
2024-05-28 01:36:08 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,35 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup =async (req,res,next)=>{
-    const {username, email, password} = req.body;
-    const hashedPassword = bcrypt.hashSync(password,10);
-    const newUser = new User({username,email,password:hashedPassword});
-    try {
-        await newUser.save(); //as this takes time so async await is used
-        res.status(201).json("User created successfully")
-        
-    } catch (error) {
-        next(error); //if error occurs it is passed to the next middleware in the index.js
-    }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save(); //as this takes time so async await is used
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error); //if error occurs it is passed to the next middleware in the index.js
+  }
+};
 
-}
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email }); //findOne method is used on User model to search for the email
+    if (!validUser) return next(errorHandler(404, "User not found!")); //middleware uses the custom error to show in UI
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //a unique thing to the user is added to token which is _id in mongoDB 
+    //remove sensitive information
+    const {password: pass, ...userWithoutPassword} = validUser._doc
+    res
+      .cookie("accessToken", token, { httpOnly: true }) //no other thirdparty app can have acces to our cookie by httpOnly 
+      .status(200)
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 //custom errorhandler
 
-export const errorhandler = (statusCode,message) =>{
+export const errorHandler = (statusCode,message) =>{
     const error = new Error(); //Error constructor is used for creating this error
     error.statusCode = statusCode;
     error.message = message;
