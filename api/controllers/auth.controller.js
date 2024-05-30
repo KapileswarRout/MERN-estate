40d1e2ef19673478338ed7bec74332049363b996
Kapileswar Rout <kapileswarrout143@gmail.com>
2024-05-30 21:39:00 +0530

    complete profile page UI and solve some errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save(); //as this takes time so async await is used
@@ -20,7 +20,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email }); //findOne method is used on User model to search for the email
     if (!validUser) return next(errorHandler(404, "User not found!")); //middleware uses the custom error to show in UI
-    const validPassword = bcrypt.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); //a unique thing to the user is added to token which is _id in mongoDB 
     //remove sensitive information from raw data
