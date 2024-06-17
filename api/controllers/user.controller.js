b37d0a472fab8bc0c98bf20bef01ed50581a8848
Kapileswar Rout <kapileswarrout143@gmail.com>
2024-06-17 10:35:24 +0530

    commit done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,64 +1,79 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js'; 
+import { errorHandler } from '../utils/error.js';
 import Listing from '../models/listing.model.js';
 
-export const test=(req,res)=>{
-    res.json({
-        message:"test api is ready"
-    });
-}
+export const test = (req, res) => {
+  res.json({
+    message: 'Api route is working!',
+  });
+};
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
-      return next(errorHandler(401, 'You can only update your own account!'));
-    try {
-      if (req.body.password) {
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
-      }
-  
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: {          //set method checks if the field is changed then only update happens else ignore the unchanged field
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            avatar: req.body.avatar,
-          },
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
         },
-        { new: true }  //it will save the updated information not the previous one
-      );
-  
-      const { password, ...rest } = updatedUser._doc;
-  
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
-  };
+      },
+      { new: true }
+    );
 
-  export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)  //params.id is the id pass here /delete/:id
-      return next(errorHandler(401, 'You can only delete your own account!'));
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
     try {
-      await User.findByIdAndDelete(req.params.id);
-      res.clearCookie('access_token'); //this delete the cookie
-      res.status(200).json('User has been deleted!');
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
     } catch (error) {
       next(error);
     }
-  };
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
 
-  export const getUserListings = async (req, res, next) => {
-    if (req.user.id === req.params.id) {
-      try {
-        const listings = await Listing.find({ userRef: req.params.id });
-        res.status(200).json(listings);
-      } catch (error) {
-        next(error);
-      }
-    } else {
-      return next(errorHandler(401, 'You can only view your own listings!'));
-    }
-  };
+export const getUser = async (req, res, next) => {
+  try {
+    
+    const user = await User.findById(req.params.id);
+  
+    if (!user) return next(errorHandler(404, 'User not found!'));
+  
+    const { password: pass, ...rest } = user._doc;
+  
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,40 +1,52 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser'
-dotenv.config()
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
 
 const app = express();
+
 app.use(express.json()); //by default json cannot be send to server so this will allow it
+
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO).then( () => {
-    console.log('Connected to mongodb')
-})
-.catch( (err)=>{
-    console.log(err);
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-
-
-app.listen(3000, ()=>{
-    console.log("server is running at port 3000!")
-})
-
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 //middleware to handle the error in auth.controller
-app.use((err,req,res,next) => {
-const statusCode = err.statusCode || 500
-const message = err.message || 'Internal Server Error'
-return res.status(statusCode).json({
-    success:false,
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
     statusCode,
-    message
-})
-})
+    message,
+  });
+});
